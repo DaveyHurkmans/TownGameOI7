51a3c5a1d671c02f9cce2235ed4c7cb9628793aa
Davey <d.hurkmans@student.fontys.nl>
2020-12-04 20:20:18 +0100

    Destroy objects
    Still have to implement a distance  check between player and object

--- Assets/Scripts/ObjectClicker.cs
+++ Assets/Scripts/ObjectClicker.cs
@@ -4,41 +4,52 @@ using UnityEngine;
 
 public class ObjectClicker : MonoBehaviour
 {
-	// click object Raycast
-	public float force = 5;
+    // click object Raycast
+    public float force = 5;
 
     // Update is called once per frame
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
-		{
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			
-			if (Physics.Raycast(ray, out hit, 5.0f))
-			{
-				if (hit.transform != null)
-				{
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // Vector3 playerDistanceToObject = Vector3.Distance(hit.transform.position, player.transform.position);
+
+            if (Physics.Raycast(ray, out hit, 20.0f))
+            {
+                if (hit.transform != null)
+                {
                     Rigidbody rb;
-                    if(rb = hit.transform.GetComponent<Rigidbody>())
+                    if (rb = hit.transform.GetComponent<Rigidbody>())
                     {
                         PrintName(hit.transform.gameObject);
-                        // LaunchIntoAir(rb);
+                        DestroyObject(hit.transform.gameObject);
                     }
-				}
-			}
-		}
+                }
+            }
+        }
     }
 
-private void PrintName(GameObject go)
-{
-	print(go.name);
-}
+    private void PrintName(GameObject go)
+    {
+        print(go.name);
+    }
 
-private void LaunchIntoAir(Rigidbody rig)
-{
-	rig.AddForce(rig.transform.up * force, ForceMode.Impulse);
-}
+    // private void LaunchIntoAir(Rigidbody rig)
+    // {
+    //     rig.AddForce(rig.transform.up * force, ForceMode.Impulse);
+    // }
+
+    private void DestroyObject(GameObject go)
+    {
+
+        // if (playerDistanceToObject >= 20.0f)
+        // {
+        Destroy(go);
+        // }
+    }
+
 
 }
 
