6552c4fd668ed502fad327846ae33926b49254ee
Davey <d.hurkmans@student.fontys.nl>
2020-12-18 11:01:37 +0100

    Beginning an inventory system

---
+++ Assets/Scripts/DefaultObject.cs
@@ -0,0 +1,12 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// [CreateAssetMenu(fileName = "New Default Object", menuName = "Inventory System/Items/Default")]
+// public class DefaultObject : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     public void Awake()
+//     {
+//         type = ItemType.Default;
+//     }
+// }
